3f39886181286e9ea381efe6c413c3a9ac2e00be
NMoore42 <nicholas.moore5@icloud.com>
2019-04-10 17:25:11 -0500

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -14,16 +14,16 @@ class Student
     # find the student in the database given a name
     # return a new instance of the Student class
   end
-  
+
   def save
     sql = <<-SQL
-      INSERT INTO students (name, grade) 
+      INSERT INTO students (name, grade)
       VALUES (?, ?)
     SQL
 
     DB[:conn].execute(sql, self.name, self.grade)
   end
-  
+
   def self.create_table
     sql = <<-SQL
     CREATE TABLE IF NOT EXISTS students (
@@ -40,4 +40,62 @@ class Student
     sql = "DROP TABLE IF EXISTS students"
     DB[:conn].execute(sql)
   end
+
+  def self.new_from_db(row)
+    new_student = Student.new
+    new_student.id = row[0]
+    new_student.name = row[1]
+    new_student.grade = row[2]
+    new_student
+  end
+
+  def self.find_by_name(name)
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE name = ?
+      LIMIT 1
+    SQL
+
+    DB[:conn].execute(sql, name).map do |row|
+      self.new_from_db(row)
+    end.first
+
+  end
+
+
+  def self.all_students_in_grade_9
+    sql = "SELECT name FROM students WHERE grade = ?;"
+
+    DB[:conn].execute(sql, 9)
+  end
+
+  def self.students_below_12th_grade
+    sql = "SELECT * FROM students WHERE grade < ? LIMIT 1;"
+
+    DB[:conn].execute(sql, 12).map {|row| self.new_from_db(row)}
+  end
+
+  def self.all
+    sql = "SELECT * FROM students;"
+
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}
+  end
+
+  def self.first_X_students_in_grade_10(qty)
+    sql = "SELECT * FROM students WHERE grade = ? LIMIT ?;"
+
+    DB[:conn].execute(sql, 10, qty).map {|row| self.new_from_db(row)}
+  end
+
+  def self.first_student_in_grade_10
+    sql = "SELECT * FROM students WHERE grade = 10 LIMIT 1"
+    DB[:conn].execute(sql).map {|row| self.new_from_db(row)}[0]
+  end
+
+  def self.all_students_in_grade_X(grade)
+    sql = "SELECT * FROM students WHERE grade = ?;"
+    DB[:conn].execute(sql, grade).map {|row| self.new_from_db(row)}
+  end
+
 end
